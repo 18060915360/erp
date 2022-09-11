550410c63e3cd6b006d80fbae72d4633ed8a2b00
wbh <731576347@qq.com>
2022-09-11 10:53:02 +0800

    erp commit

--- bdqn/src/main/java/control/PersonController.java
+++ bdqn/src/main/java/control/PersonController.java
@@ -2,6 +2,11 @@ package control;
 
 public class PersonController {
     public void getPersonList(){
-        System.out.println("erp2 getPersonList");
+        System.out.println("erp2 getPersonList111");
+        System.out.println("22ssasdsadas");
+    }
+
+    public void test02(){
+        System.out.println("22222dsada");
     }
 }
